586489d1d669ba0e4f74a576f47d657b79ee4ef9
issam1994 <issamaitouahmane@gmail.com>
2021-01-20 11:25:07 +0000

    updated the project setup. now using parcel for serving files and handling update Refactored how the project files are organized. did some renaming. added and removed some pkgs.

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: [
+    require('postcss-import'),
     require('tailwindcss'),
     require('postcss-preset-env')({ stage: 1 }),
     ...process.env.NODE_ENV === 'production'
