23f9c8604102691d86af66260e7bc9a9854c8840
issam1994 <issamaitouahmane@gmail.com>
2021-06-08 15:54:47 +0100

    updated the installed libs and added some more tailwindcss plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: ['./public/**/*.html', './public/**/*.js'],
+  purge: ["./public/**/*.html", "./public/**/*.js"],
+  darkMode: false, // or 'media' or 'class',
+  corePlugins: {
+    container: false,
+  },
   theme: {
     extend: {
       colors: {
@@ -8,45 +12,58 @@ module.exports = {
           blue67: "#677FFF",
           green2f: "#2FEAB5",
           orangeff8: "#FF894A",
-        }
+        },
       },
       spacing: {
-        // plugin by issam 
+        // plugin by issam
         ...(() => {
-          let utilities = {}
+          let utilities = {};
           for (let i = 0; i <= 128; i++) {
             utilities[`${i}-px`] = `${i * 0.0625}rem`;
           }
-          return utilities
-        })()
+          return utilities;
+        })(),
       },
       fontSize: {
         ...(() => {
-          let utilities = {}
+          let utilities = {};
           for (let i = 0; i <= 128; i++) {
             utilities[`${i}-px`] = `${i * 0.0625}rem`;
           }
-          return utilities
-        })()
+          return utilities;
+        })(),
       },
       screens: {
-        'max-2xl': { 'max': '1535px' },
+        "max-2xl": { max: "1535px" },
         // => @media (max-width: 1535px) { ... }
 
-        'max-xl': { 'max': '1279px' },
+        "max-xl": { max: "1279px" },
         // => @media (max-width: 1279px) { ... }
 
-        'max-lg': { 'max': '1023px' },
+        "max-lg": { max: "1023px" },
         // => @media (max-width: 1023px) { ... }
 
-        'max-md': { 'max': '767px' },
+        "max-md": { max: "767px" },
         // => @media (max-width: 767px) { ... }
 
-        'max-sm': { 'max': '639px' },
+        "max-sm": { max: "639px" },
         // => @media (max-width: 639px) { ... }
       },
     },
   },
   variants: {},
-  plugins: [],
-}
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms")({
+      // strategy: 'class',
+    }),
+    require("tailwind-bootstrap-grid")({
+      containerMaxWidths: {
+        sm: "540px",
+        md: "720px",
+        lg: "960px",
+        xl: "1140px",
+      },
+    }),
+  ],
+};
